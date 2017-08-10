887d08014cc4f771be3dbd5e1e8769c5915ff1e5
Estudiante <Estudiante@10.203.0.231>
2017-08-10 14:18:15 -0500

    ---

--- src/holamundo/HolaMundo.java
+++ src/holamundo/HolaMundo.java
@@ -16,7 +16,7 @@ public class HolaMundo {
      */
     public static void main(String[] args) {
         for (int i = 0; i < 0; i++) {
-            System.out.println("Hola Mundo");
+            System.out.println("Hola Diego");
         }
         
 
